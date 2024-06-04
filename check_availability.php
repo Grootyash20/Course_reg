aa5bb68dcbf58b47b9678bdfb6ad0094fe62a659
Grootyash20 <128805588+Grootyash20@users.noreply.github.com>
2024-06-04 19:19:49 +0530

    Update check_availability.php

--- check_availability.php
+++ check_availability.php
@@ -2,7 +2,7 @@
 require_once("includes/config.php");
 if(!empty($_POST["cid"])) {
 $cid= $_POST["cid"];
- $regid=$_SESSION['login'];
+ $regid=$_SESSION['login']; 
 		$result =mysqli_query($con,"SELECT studentRegno FROM 	courseenrolls WHERE course='$cid' and studentRegno=' $regid'");
 	$count=mysqli_num_rows($result);
 if($count>0) 
