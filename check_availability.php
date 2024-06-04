a366fe2cf37ff181b93ab5c13f9721fdb2837838
Grootyash20 <128805588+Grootyash20@users.noreply.github.com>
2024-06-04 19:19:20 +0530

    Update check_availability.php

--- check_availability.php
+++ check_availability.php
@@ -5,7 +5,7 @@ $cid= $_POST["cid"];
  $regid=$_SESSION['login'];
 		$result =mysqli_query($con,"SELECT studentRegno FROM 	courseenrolls WHERE course='$cid' and studentRegno=' $regid'");
 	$count=mysqli_num_rows($result);
-if($count>0)
+if($count>0) 
 {
 echo "<span style='color:red'> Already Applied for this course.</span>";
  echo "<script>$('#submit').prop('disabled',true);</script>";
