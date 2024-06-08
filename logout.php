98171ef3993e1856b08f0b04ad0f91e9518decda
Grootyash20 <128805588+Grootyash20@users.noreply.github.com>
2024-06-09 01:10:50 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include("includes/config.php");
-$_SESSION['login']=="";
+$_SESSION['login']==""; 
 date_default_timezone_set('Asia/Kolkata'); 
 $ldate=date( 'd-m-Y h:i:s A', time () ); 
 mysqli_query($con,"UPDATE userlog  SET logout = '$ldate' WHERE studentRegno = '".$_SESSION['login']."' ORDER BY id DESC LIMIT 1");
